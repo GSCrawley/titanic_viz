fc7c5f45fbf2ee827a58e3307a6839517d0cfc61
GSCrawley <gscrawley@gmail.com>
2021-02-06 02:33:57 -0800

    added stylesheet, fonts

--- script.js
+++ script.js
@@ -1,88 +1,276 @@
-import data from './titanic-data.js'
+// Load Titanic Passenger Data --------------------
+fetch('passengers.json')
+	.then((res) => res.json())
+	.then((json) => handleData(json))
+	.catch((err) => console.log(err.message));
 
-const titanic = document.querySelector('#titanic')
+// Variables ---------------------------------------
+let state = false;
+let showGender = false;
+let showCasualties = false;
+let showChildren = false;
+let showClass = false;
+let showFare = false;
+const elements = [];
+const passengerData = [];
 
-titanic.style.display = 'grid'
-titanic.style.gridTemplateColumns = 'repeat(20, 10px)'
-titanic.style.gridGap = '1px'
+// Handle Data -------------------------------------
+function handleData(json) {
+	const fields = json.map((passengers) => passengers.fields);
 
-const passengers = data.map(p => {
-  return document.createElement('div')
-})
+	// Summary of Data --------------------------------
+	// Total Passengers
+	const totalPassengers = fields.length;
+	document.getElementById('total-passengers').innerHTML = totalPassengers;
+	console.log('Total Passengers:' + totalPassengers);
 
-passengers.forEach(p => {
-  titanic.appendChild(p)
-})
+	// Survivors
+	const Survivors = fields.filter((passenger) => {
+		return passenger.survived === 'Yes';
+	});
+	document.getElementById('survivors').innerHTML = Survivors.length;
+	console.log('Number of Survivors:' + Survivors.length);
 
-passengers.forEach((p, i) => {
-  const el = document.createElement('div')
-  titanic.appendChild(el)
-  el.style.width = '20px'
-  el.style.height = '20px'
-  el.style.border = '1px solid'
-  el.style.margin = '1px'
-  el.style.backgroundColor = p.fields.survived === 'Yes' ? 'tomato' : 'grey'
-  el.style.borderRadius = p.fields.sex === 'female' ? '50%' : '0'
-})
+	// Deaths ------------------------------------
+	const Casualties = fields.filter((passenger) => {
+		return passenger.survived === 'No';
+	});
+	document.getElementById('deaths').innerHTML = Casualties.length;
+	console.log('Number of Casualties:' + Casualties.length);
 
-// Get a reference to the #titanic
+	// Child Passengers (Under 13) ------------------
+	const childPassengers = fields.filter((passenger) => {
+		return passenger.age < 13;
+	});
+	document.getElementById('childPassengers').innerHTML = childPassengers.length;
+	console.log('Total Number of Child Passengers: ' + childPassengers.length);
 
+	// Number of Female Passengers -------------------
+	const femalePassengers = fields.filter((passenger) => {
+		return passenger.sex === 'female';
+	});
+	document.getElementById('femalePassengers').innerHTML = femalePassengers.length;
+	console.log('Female Deaths: ' + femaleDeaths.length);
 
-// Set some styles on the titanic
-// display flex, justifyContent center, alignItems flex-end
-titanic.style.display = 'grid'
-titanic.style.gridTemplateColumns = 'repeat(20, 10px)'
-titanic.style.gridGap = '1px'
+	// Number of Male Passengers ---------------------
+	const malePassengers = fields.filter((passenger) => {
+		return passenger.sex === 'male';
+	});
+	document.getElementById('malePassengers').innerHTML = malePassengers.length;
+	console.log('Male Deaths: ' + maleDeaths.length);
 
-// Map over the data and make a new element for each passenger
-const passengers = data.map(p => {
-  return document.createElement('div')
-})
+	// Male Deaths ----------------------------------
+	const deadMen = malePassengers.reduce((acc, pass) => {
+		if (pass.survived === 'No') {
+			acc += 1;
+		}
+		return acc;
+	}, 0);
+	document.getElementById('maleDeaths').innerHTML = deadMen;
+	console.log('Male Deaths: ' + deadMen);
 
-// Loop over each passenger and append them to the titanic
-  
-// Let's loop over each passenger and set some styles 
-passengers.forEach((p, i) => {
-  p.classList.add('passenger')
-p.style.width = '15px'
-p.style.height = '15px'
-p.style.borderRadius = data[i].fields.sex === 'female'
-p.style.opacity  = data[i].fields.survived === 'Yes'
-p.style.backgroundColor = portColor[data[i].fields]
-})
+	// Female Deaths --------------------------------
+	const deadWomen = femalePassengers.reduce((acc, pass) => {
+		if (pass.survived === 'No') {
+			acc += 1;
+		}
+		return acc;
+	}, 0);
+	document.getElementById('femaleDeaths').innerHTML = deadWomen;
+	console.log('Female Deaths: ' + deadWomen);
 
-})
-  // Challenges - 
+	// Child Deaths (Under 13) ----------------------
+	const deadChildren = childPassengers.reduce((acc, child) => {
+		if (child.survived === 'No') {
+			acc += 1;
+		}
+		return acc;
+	}, 0);
+	document.getElementById('childDeaths').innerHTML = deadChildren;
+	console.log('Child Deaths: ' + deadChildren);
 
-// Make the squares a little bigger 15px by 15px. 
-// You'll need to change both the gridTemplateColumn on the
-// titanic and the width and height of each passenger. 
+	// Challenge 5: Passenger Classes
+	// const pClasses = new Set()
+	// const numOfPClasses
+	// const pc = {}
+	// fields.forEach((passenger) => {
+	//   pClasses.add(passenger.pclass)
+	//   pc[passenger.pclass]
+	// })
 
+	// Access Name, Fare, PClass, Survived Age ------
+	const allFares = fields.filter((passenger) => passenger.fare !== undefined).map(({ fare }) => fare);
+	console.log('All Fares:' + allFares);
 
+	// Ages ----------------------------------------
+	const allAges = fields
+		.filter((passenger) => {
+			return passenger.age !== undefined;
+		})
+		.map((passenger) => {
+			return passenger.age;
+		});
 
-// Change the number of columns on the titanic to 34
+	const minAge = Math.min(...allAges);
+	const maxAge = Math.max(...allAges);
+	const ageRange = maxAge - minAge;
 
+	document.getElementById('youngest-passenger').innerHTML = minAge;
 
-// Display each passenger as a circle if they are female. 
-// Do this by setting the borderRadius of each passenger. 
-// Match the passenger in passengers to the object data 
-// in the data array by the index. 
+	document.getElementById('oldest-passenger').innerHTML = maxAge;
 
+	console.log('All Ages:' + allAges);
+	console.log('Min Age:' + minAge);
+	console.log('Max Age:' + maxAge);
+	console.log('Age Range:' + ageRange);
 
+	renderPassengers(fields, 'render-all-passengers');
+	renderPassengers(fields, 'render-male-passengers');
 
-// Display each passengers who did not survive as 
-// opacity 0.5. 
+	// Render Passenger Squares -------------------------
+	renderPassengers(fields, 'render-all-passengers');
 
+	function renderPassengers(data, id) {
+		const root = document.getElementById(id);
 
+		root.style.display = 'flex';
+		root.style.flexWrap = 'wrap';
 
-// Set the backgroundColor of each passenger by their 
-// embarked value. There are three possible values: 
-// 'S', 'C', and 'Q'
+		data.forEach((passenger, i) => {
+			const el = document.createElement('div'); // Make element and attach to DOM
+			root.appendChild(el);
+			elements.push(el); //store element
+			passengerData.push(passenger); //store passenger
 
-const passengerDetails = document.querySelector('#passengerDetails')
+			el.classList.add('square-styles');
+			el.dataset.index = i; // <div data-index="i">
+		});
+	}
+}
 
-document.body.addEventListener('mouseover', (e) => {
-  if (e.target.matches('.passenger')) {
-console.log('mouse enter passenger!')
-  }
-})
+//  Passenger Event Listener -------------------------------
+const body = document.querySelector('body'); // qs will select 1st instance
+body.addEventListener('click', (e) => {
+	//always takes event object as parameter
+	const index = e.target.dataset.index;
+	console.log(index);
+
+	if (index !== undefined) showOverlay(index);
+	console.log(passengerData[index]);
+});
+
+function showOverlay(index) {
+	const el = document.getElementById('showOverlay');
+	const { name, sex, age, survived, fare, pclass } = passengerData[index];
+	el.style.display = 'block';
+	el.innerHTML = `
+  <div><span><strong>Name: </strong></span>${name}</div>
+  <div><span><strong>Gender: </strong></span>${sex}</div>
+  <div><span><strong>Age: </strong></span>${age}</div>
+  <div><span><strong>Survived: </strong></span>${survived}</div>
+  <div><span><strong>Passenger Class: </strong></span>${pclass}</div>
+  <div><span><strong>Fare: </strong></span>${fare}</div>
+   `;
+	el.style.height = '140px';
+	el.style.width = '220px';
+	el.style.margin = '3px';
+	el.style.border = '2px solid black';
+}
+
+// Toggle Gender Button On/Off -----------------------------
+const showGenderButton = document.getElementById('showGenderButton');
+showGenderButton.addEventListener('click', (e) => {
+	showGender = !showGender;
+
+	if (showGender) {
+		e.target.style.backgroundColor = '#3355a3';
+		e.target.style.color = 'white';
+		e.target.classList.add('buttonActive');
+		e.target.innerHTML = 'Reset';
+		// show gender in el
+		displayGender();
+	} else {
+		e.target.style.backgroundColor = 'white';
+		e.target.style.color = 'black';
+		e.target.classList.remove('buttonActive');
+		e.target.innerHTML = 'Gender';
+
+		displayGender();
+	}
+});
+
+//  Display Gender -------------------------------
+function displayGender() {
+	console.log(showGender);
+	passengerData.forEach((obj, i) => {
+		// console.log(obj)
+		const el = elements[i];
+		let color = obj.sex === 'male' ? '#3355a3' : '#ab3e32';
+		if (!showGender) {
+			color = '#2b2b2b';
+		}
+		el.style.backgroundColor = color;
+	});
+}
+
+// Toggle Casualties Button On/ Off -----------------
+const showCastualtiesButton = document.getElementById('showCasualtiesButton');
+showCasualtiesButton.addEventListener('click', (e) => {
+	showCasualties = !showCasualties;
+
+	if (showCasualties) {
+		e.target.style.backgroundColor = '#fdffba';
+		e.target.classList.add('buttonActive');
+		displayCasualties();
+		e.target.innerHTML = 'Reset';
+	} else {
+		e.target.style.backgroundColor = 'white';
+		e.target.classList.remove('buttonActive');
+		displayCasualties();
+		e.target.innerHTML = 'Casualties';
+	}
+});
+
+// Display Casualties---------------------------
+function displayCasualties() {
+	console.log(showCasualties);
+	passengerData.forEach((obj, i) => {
+		const el = elements[i];
+		el.innerHTML = obj.survived === 'No' ? '☠︎' : '';
+		if (!showCasualties) {
+			el.innerHTML = '';
+		}
+	});
+}
+
+// Toggle Childen Button On/Off -----------------------------
+const showChildrenButton = document.getElementById('showChildrenButton');
+showChildrenButton.addEventListener('click', (e) => {
+	showChildren = !showChildren;
+	if (showChildren) {
+		e.target.classList.add('buttonActive');
+		e.target.innerHTML = 'Reset';
+		displayChildren();
+	} else {
+		e.target.classList.remove('buttonActive');
+		e.target.innerHTML = 'Gender';
+	}
+});
+
+//  Display Children -------------------------------
+function displayChildren() {
+	// const childPassengers = fields.filter((passenger) => {
+	// 	return passenger.age < 13;
+	// });
+
+	console.log('showChildren');
+	passengerData.forEach((obj, i) => {
+		const el = elements[i];
+		let radius = obj.age < 13 ? '50%' : '0%';
+		// console.log(color);
+		el.style.borderRadius = radius;
+		// el.innerHTML = obj.childPassengers === 'True' ? #;
+		if (!showChildren) {
+			el.style.borderRadius = '0%';
+		}
+	})
